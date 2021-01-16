c85c6946a4408d1f9219441ae3f7aed679b10458
zhengsipeng <121569502@qq.com>
2021-01-16 12:45:13 +0000

    add gen_miss_json

--- extract_miss_kinetics.py
+++ extract_miss_kinetics.py
@@ -1,14 +1,15 @@
 import os
 import sys
+import json
 from tqdm import tqdm
 
-
-if not os.path.exists('kinetics_mini'):
-    os.makedirs('kinetics_mini')
-if not os.path.exists('kinetics_400'):
-    os.makedirs('kinetics_400')
     
 def main(is_mini, vpath):
+    if not os.path.exists('kinetics_mini'):
+        os.makedirs('kinetics_mini')
+    if not os.path.exists('kinetics_400'):
+        os.makedirs('kinetics_400')
+
     vids = [l.split('.')[0] for l in os.listdir(vpath)]
     for split in ['train', 'val', 'test']:
         if int(is_mini) == 0:
@@ -27,7 +28,27 @@ def main(is_mini, vpath):
                 os.system(command)
 
 
+def gen_miss_json(split):
+    with open('resources/origin/kinetics_%s.json'%split, 'r') as f:
+        annos = json.load(f)
+    with open('resources/mini_miss_val.list', 'r') as f:
+        misslist = [l.split('/')[-1][:11] for l in f.readlines()]
+    with open('resources/mini_miss_test.list', 'r') as f:
+        misslist += [l.split('/')[-1][:11] for l in f.readlines()]
+    with open('resources/mini_miss_train.list', 'r') as f:
+        misslist += [l.split('/')[-1][:11] for l in f.readlines()]
+    print(len(misslist))
+    new_anno = dict()
+    for vid in tqdm(annos):
+        if vid in misslist:
+            new_anno[vid] = annos[vid]
+    print(len(new_anno.keys()))
+    with open('resources/kinetics_%s.json'%split, 'w') as f:
+        json.dump(new_anno, f)
+
+
 if __name__ == '__main__':
-    is_mini = sys.argv[1]
-    vpath = sys.argv[2]
-    main(is_mini, vpath)
+    #is_mini = sys.argv[1]
+    #vpath = sys.argv[2]
+    #main(is_mini, vpath)
+    gen_miss_json('train')
